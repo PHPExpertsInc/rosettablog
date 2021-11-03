5c7a4519b5c8eab9a988e56ca41b6a349a8e266c
Theodore R. Smith <theodore@phpexperts.pro>
2021-11-03 05:25:05 +0100

    Added the config file.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$config['blog_name'] = 'incendiary.ws';
+$config['mission'] = 'A needle of <em>Hope</em> amid a haystack of <em>Chaos</em>.';
+
+// blog_platform: Rosetta, Drupal5, Drupal6
+$config['blog_platform'] = 'Drupal5';
+
+// url_style: Rosetta, Drupal
+$config['url_style'] = 'Drupal';
+$config['pretty_urls'] = true;
+
+// theme_platform: Rosetta, Drupal5, Drupal6, Drupal7
+$config['theme_platform'] = 'Rosetta';
+$config['theme'] = 'garland';
+
+return $config;
